57a1ac048ec28c31d15057942c2b4a1c4483004c
elpinal <elpinal@users.noreply.github.com>
2017-10-01 19:08:36 +0900

    Add Language

--- src/Explicit/Usage.hs
+++ src/Explicit/Usage.hs
@@ -1,1 +1,11 @@
+{-# LANGUAGE GADTs #-}
+
 module Explicit.Usage where
+
+data Language a where
+  Symbol :: Monoid m => m -> Language m
+  Kleene :: Monoid m => Language m -> Language m
+
+toString :: Language String -> String
+toString (Symbol m) = m
+toString (Kleene l) = toString l `mappend` "*"
--- test/Explicit/UsageSpec.hs
+++ test/Explicit/UsageSpec.hs
@@ -2,5 +2,10 @@ module Explicit.UsageSpec where
 
 import Test.Hspec
 
+import Explicit.Usage
+
 spec :: Spec
-spec = return ()
+spec = do
+  describe "toString" $
+    it "stringify Language" $
+      toString (Kleene (Symbol "a")) `shouldBe` "a*"
