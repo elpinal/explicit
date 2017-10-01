7bb92d8e912b7fa035b6579aa8e1807ce28947ae
elpinal <elpinal@users.noreply.github.com>
2017-10-01 19:32:08 +0900

    Add Concat

--- src/Explicit/Usage.hs
+++ src/Explicit/Usage.hs
@@ -9,15 +9,18 @@ data Language a where
   Kleene :: Monoid m => Language m -> Language m
   Positive :: Monoid m => Language m -> Language m
   Union :: Monoid m => Language m -> Language m -> Language m
+  Concat :: Monoid m => Language m -> Language m -> Language m
 
 toString :: Language String -> String
 toString (Symbol m) = m
 toString (Kleene l) = toString l <> "*"
 toString (Positive l) = toString l <> "+"
 toString (Union l m) = toString l <> "|" <> toString m
+toString (Concat l m) = toString l <> toString m
 
 toUsageString :: Language String -> String
 toUsageString (Symbol m) = show m
 toUsageString (Kleene l) = "[" <> toUsageString l <> "...]"
 toUsageString (Positive l) = toUsageString l <> "..."
 toUsageString (Union l m) = toUsageString l <> "|" <> toUsageString m
+toUsageString (Concat l m) = toUsageString l <> " " <> toUsageString m
