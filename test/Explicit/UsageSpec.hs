fb7436813704c0b9669776a6ff87bfd9be631ac9
elpinal <elpinal@users.noreply.github.com>
2017-10-02 23:52:18 +0900

    Unify test for 'format' into one for 'display'

--- test/Explicit/UsageSpec.hs
+++ test/Explicit/UsageSpec.hs
@@ -5,16 +5,14 @@ import Test.Hspec
 import Explicit.Usage
 
 spec :: Spec
-spec = do
-  describe "format" $
-    it "stringify Language suitably for usage" $ do
-      format (Kleene . Symbol $ Literal "a") `shouldBe` Meta "[\"a\"...]"
-      format (Positive . Symbol $ Meta "a") `shouldBe` Meta "a..."
-      format exampleUsage `shouldBe` Meta "\"git\" flags (\"clone\" | \"init\")"
-
+spec =
   describe "display" $
     it "formats things into user-facing output" $ do
       display (Kleene $ Symbol "a") `shouldBe` "a*"
+      display (Kleene . Symbol $ Literal "a") `shouldBe` "[\"a\"...]"
+      display (Positive . Symbol $ Meta "a") `shouldBe` "a..."
+      display exampleUsage `shouldBe` "\"git\" flags (\"clone\" | \"init\")"
+
       display flagDef `shouldBe` "flags = \"--help\" | \"--version\""
       display (Usage exampleUsage [flagDef]) `shouldBe` unlines [display exampleUsage, "", display flagDef]
 
