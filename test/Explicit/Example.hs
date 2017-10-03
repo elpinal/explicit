3fdcf228ef92f732e641a18472355e02ab16a104
elpinal <elpinal@users.noreply.github.com>
2017-10-03 21:18:13 +0900

    Add help in Explicit.Example

--- test/Explicit/Example.hs
+++ test/Explicit/Example.hs
@@ -2,10 +2,24 @@ module Explicit.Example where
 
 import Control.Monad.Writer.Lazy
 
+import Explicit.Usage
+
 run :: [String] -> Writer [String] ()
 run args = do
   tell [ "Hello!"
        , "This is an example program for testing of Explicit module."
        , "The arguments are processed as filenames"
        ]
-  tell args
+  case args of
+    ("-h" : _) -> help
+    _ -> tell args
+
+help :: Writer [String] ()
+help = do
+  tell [ "The example program."
+       , "This message is displayed to indicate usage."
+       ]
+  tell [display usage]
+
+usage :: Language Alphabet
+usage = lit "example" #- (lit "-h" |- Kleene (meta "filepath"))
